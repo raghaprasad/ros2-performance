16091e17613beecbc8c793b2ee9d9397ef077c32
Ragha Prasad <prasadra@amazon.com>
2019-04-25 15:08:16 -0700

    ..

--- performance_test/pub_sub_1p_5s.sh
+++ performance_test/pub_sub_1p_5s.sh
@@ -1,6 +1,6 @@
 source env.sh
 export MAX_PUBLISHERS=1
-export MAX_SUBSCRIBERS=5
+export MAX_SUBSCRIBERS=1
 export MSG_TYPES="10b 100b 250b 1kb 10kb 100kb 250kb 1mb 4mb 8mb"
 export PUBLISH_FREQUENCY=1
 export DURATION=10
